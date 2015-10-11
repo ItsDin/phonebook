d11898b562887aaced60c24c6fcbfc5243fe43b1
ItsDin <imdindini88@gmail.com>
2015-10-12 00:47:19 +0800

    A linked list find lastname one by one
    This is original version find name one by one by linked list.
    The method of mod idea has not yet been completed.

--- phonebook_opt.c
+++ phonebook_opt.c
@@ -2,14 +2,25 @@
 
 #include "phonebook_opt.h"
 
-/* FILL YOUR OWN IMPLEMENTATION HERE! */
+/* o version */
 entry *findName(char lastname[], entry *pHead)
 {
-    /* TODO: implement */
+    /* TODO: find name one by one */
+    while(pHead != NULL){
+        if(strcasecmp(lastname, pHead->lastName) == 0)
+            return pHead;
+        pHead = pHead -> pNext;
+    }
     return NULL;
 }
 
 entry *append(char lastName[], entry *e)
 {
-    return NULL;
+    e->pNext = (entry *) malloc(sizeof(entry));
+    e = e->pNext;
+    strcpy(e->lastName, lastName);
+    e->pNext = NULL;
+
+    return e;
+//    return NULL;
 }
