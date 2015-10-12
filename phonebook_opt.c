2b106635d6576fc651c1819b4e244af124136db5
ItsDin <imdindini88@gmail.com>
2015-10-12 20:47:44 +0800

    Add the HASH function.
    This code that I add the hash function to test.
    But It has not been completed.

--- phonebook_opt.c
+++ phonebook_opt.c
@@ -2,10 +2,25 @@
 
 #include "phonebook_opt.h"
 
-/* o version */
+/* hash version */
+entry *hash_table[HASH_SIZE];
+entry *hash_array[HASH_SIZE];
+
+static inline unit32_t hashFind(char lastname[])
+{
+    int number;
+    number = 0;
+    while(*lastname)
+        number += *lastname++;
+    return (number%HASH_SIZE);
+}
+
 entry *findName(char lastname[], entry *pHead)
 {
-    /* TODO: find name one by one */
+    /* TODO: find name with hash */
+    int h_value = hashFind(lastname);
+    pHead = hash_table[h_value];
+
     while(pHead != NULL){
         if(strcasecmp(lastname, pHead->lastName) == 0)
             return pHead;
@@ -16,11 +31,27 @@ entry *findName(char lastname[], entry *pHead)
 
 entry *append(char lastName[], entry *e)
 {
-    e->pNext = (entry *) malloc(sizeof(entry));
-    e = e->pNext;
-    strcpy(e->lastName, lastName);
-    e->pNext = NULL;
+    int bucket = hashFind(lastName);
+    entry *new_one = (entry *)malloc(sizeof(entry));
+    
+    if(hash_table[bucket]){
+        hash_array[bucket] -> pNext = new_one;
+        new_one -> pNext = NULL;
+        strcpy(new_one -> lastName, lastName);
+        hash_array[bucket] = new_one;
+    }else{
+        new_one -> pNext = NULL;
+        strcpy(new_one -> lastName, lastName);
+        hash_array[bucket] = new_one;
+        hash_table[bucket] = new_one;
+    }
+    return new_one;
 
-    return e;
+//    e->pNext = (entry *) malloc(sizeof(entry));
+//    e = e->pNext;
+//    strcpy(e->lastName, lastName);
+//    e->pNext = NULL;
+
+//    return e;
 //    return NULL;
 }
