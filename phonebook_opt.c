5f201bb861007c8677f766e4d3a508166c3e5b91
ItsDin <imdindini88@gmail.com>
2015-10-18 00:46:01 +0800

    Modify the naming and release memory
    1. hashFind -> hashTablePhone
    2. release memory
    3. pointer arrow modify "e -> pNext" to "e->pNext"

--- phonebook_opt.c
+++ phonebook_opt.c
@@ -9,7 +9,7 @@
 entry *hash_table[HASH_SIZE];
 entry *hash_array[HASH_SIZE];
 
-static inline int hashFind(char lastname[])
+static inline int hashTablePhone(char lastname[])
 {
     int number;
     number = 0;
@@ -21,33 +21,34 @@ static inline int hashFind(char lastname[])
 entry *findName(char lastname[], entry *pHead)
 {
     /* TODO: find name with hash */
-    int h_value = hashFind(lastname);
+    int h_value = hashTablePhone(lastname);
     pHead = hash_table[h_value];
 
     while(pHead != NULL){
         if(strcasecmp(lastname, pHead->lastName) == 0)
             return pHead;
-        pHead = pHead -> pNext;
+        pHead = pHead->pNext;
     }
     return NULL;
 }
 
 entry *append(char lastName[], entry *e)
 {
-    int bucket = hashFind(lastName);
+    int bucket = hashTablePhone(lastName);
 
     e->pNext = (entry *) malloc(sizeof(entry));
-    e = e -> pNext;
+    e = e->pNext;
     if(hash_table[bucket]){
-        hash_array[bucket] -> pNext = e;
+        hash_array[bucket]->pNext = e;
         e -> pNext = NULL;
         strcpy(e -> lastName, lastName);
         hash_array[bucket] = e;
     }else{
-        e -> pNext = NULL;
-        strcpy(e -> lastName, lastName);
+        e->pNext = NULL;
+        strcpy(e->lastName, lastName);
         hash_array[bucket] = e;
         hash_table[bucket] = e;
     }
+    free(e->pNext);
     return e;
 }
